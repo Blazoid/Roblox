bfab710d0790c06def490c579e68ec0fd04bb880
Blazoid <tresh.lol@mail.ru>
2025-09-13 12:42:42 +0400

    Update custom_fonts.lua

--- storage/custom_fonts.lua
+++ storage/custom_fonts.lua
@@ -20,15 +20,15 @@ do
         if not isfile(asset) then
             writefile(asset, Asset.Font)
         end
-        writefile(font,game:GetService('HttpService'):JSONEncode({name = Name,faces = {{name = 'Regular',weight = Weight,style = Style,assetId = getcustomasset(asset),},},}))
+        writefile(font,game:GetService('HttpService'):JSONEncode({name = Name,faces = {{name = 'Regular',weight = Weight,style = Style,assetId = getcustomasset(asset)}}}))
         return getcustomasset(font)
     end
     Flags.Fonts = {
-        Graph_35 = Font.new(Flags.Trash:Register_Font('Graph_35', 400, 'Regular', {Id = 'Graph_35.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Graph_35')),})),
-        Minecraftia = Font.new(Flags.Trash:Register_Font('Minecraftia', 400, 'Regular', {Id = 'Minecraftia.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Minecraftia')),})),
-        ProggyCleanTT = Font.new(Flags.Trash:Register_Font('ProggyCleanTT', 400, 'Regular', {Id = 'ProggyCleanTT.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyCleanTT')),})),
-        Smallest_Pixel = Font.new(Flags.Trash:Register_Font('Smallest_Pixel', 400, 'Regular', {Id = 'Smallest_Pixel.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Smallest_Pixel')),})),
-        Templeos = Font.new(Flags.Trash:Register_Font('Templeos', 400, 'Regular', {Id = 'Templeos.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Templeos')),})),
-        ProggyTiny = Font.new(Flags.Trash:Register_Font('ProggyTiny', 400, 'Regular', {Id = 'ProggyTiny.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyTiny')),})),
+        Graph_35 = Font.new(Flags.Trash:Register_Font('Graph_35', 400, 'Regular', {Id = 'Graph_35.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Graph_35'))})),
+        Minecraftia = Font.new(Flags.Trash:Register_Font('Minecraftia', 400, 'Regular', {Id = 'Minecraftia.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Minecraftia'))})),
+        ProggyCleanTT = Font.new(Flags.Trash:Register_Font('ProggyCleanTT', 400, 'Regular', {Id = 'ProggyCleanTT.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyCleanTT'))})),
+        Smallest_Pixel = Font.new(Flags.Trash:Register_Font('Smallest_Pixel', 400, 'Regular', {Id = 'Smallest_Pixel.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Smallest_Pixel'))})),
+        Templeos = Font.new(Flags.Trash:Register_Font('Templeos', 400, 'Regular', {Id = 'Templeos.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Templeos'))})),
+        ProggyTiny = Font.new(Flags.Trash:Register_Font('ProggyTiny', 400, 'Regular', {Id = 'ProggyTiny.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyTiny'))})),
     }
 end
