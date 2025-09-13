3fb620f52ff5c2c8b7d8abb5b4d7b9cbf487aa1d
Blazoid <tresh.lol@mail.ru>
2025-09-13 13:00:48 +0400

    Update create.lua

--- storage/create.lua
+++ storage/create.lua
@@ -1,2 +1,3 @@
+--// Example: local hitsound = create:create("Sound", services.SoundService, { SoundId = "rbxassetid://6607339542", Volume = 5 })
 function create(instance, parent, options) local ins = Instance.new(instance, parent) ins.Name = " " for prop, value in next, options do ins[prop] = value end return ins end
 return create
