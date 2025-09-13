f62311f19266aa79f409ded211fe4fa87e0eaf76
Blazoid <tresh.lol@mail.ru>
2025-09-13 13:20:58 +0400

    Update custom_fonts.lua

--- storage/custom_fonts.lua
+++ storage/custom_fonts.lua
@@ -1,19 +1,16 @@
---// Example: TextLabel.FontFace = Flags.Library.Fonts[font name]
-if not getgenv().Flags then getgenv().Flags = {} elseif not getgenv().Flags.Library then getgenv().Flags.Library = {} elseif not getgenv().Flags.Library.Fonts then getgenv().Flags.Library.Fonts = {} end
-if not getgenv().Flags then getgenv().Flags = {} elseif not getgenv().Flags.Library then getgenv().Flags.Library = {} elseif not getgenv().Flags.Library.Trash then getgenv().Flags.Library.Trash = {} end
-
-getgenv().Flags.Library.Fonts = {
+--// Example: TextLabel.FontFace = custom_fonts[font name]
+if not getgenv().custom_fonts then getgenv().custom_fonts = {} end
+getgenv().custom_fonts = {
     Graph_35 = {},
     Minecraftia = {},
     ProggyCleanTT = {},
     Smallest_Pixel = {},
     Templeos = {},
     ProggyTiny = {},
+	Trash_Bag = {}
 }
-getgenv().Flags.Library.Trash = {}
-
 do
-    function Flags.Library.Trash:Register_Font(Name, Weight, Style, Asset)
+    function custom_fonts.Trash_Bag:Register_Font(Name, Weight, Style, Asset)
         if not isfolder('Fonts') then makefolder('Fonts') end
         local asset = 'Fonts/' .. Asset.Id
         local font = 'Fonts/' .. Name .. '.font'
@@ -21,13 +18,13 @@ do
         writefile(font,game:GetService('HttpService'):JSONEncode({name = Name,faces = {{name = 'Regular',weight = Weight,style = Style,assetId = getcustomasset(asset)}}}))
         return getcustomasset(font)
     end
-    Flags.Library.Fonts = {
-        Graph_35 = Font.new(Flags.Library.Trash:Register_Font('Graph_35', 400, 'Regular', {Id = 'Graph_35.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Graph_35'))})),
-        Minecraftia = Font.new(Flags.Library.Trash:Register_Font('Minecraftia', 400, 'Regular', {Id = 'Minecraftia.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Minecraftia'))})),
-        ProggyCleanTT = Font.new(Flags.Library.Trash:Register_Font('ProggyCleanTT', 400, 'Regular', {Id = 'ProggyCleanTT.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyCleanTT'))})),
-        Smallest_Pixel = Font.new(Flags.Library.Trash:Register_Font('Smallest_Pixel', 400, 'Regular', {Id = 'Smallest_Pixel.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Smallest_Pixel'))})),
-        Templeos = Font.new(Flags.Library.Trash:Register_Font('Templeos', 400, 'Regular', {Id = 'Templeos.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Templeos'))})),
-        ProggyTiny = Font.new(Flags.Library.Trash:Register_Font('ProggyTiny', 400, 'Regular', {Id = 'ProggyTiny.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyTiny'))})),
+    custom_fonts = {
+        Graph_35 = Font.new(custom_fonts.Trash_Bag:Register_Font('Graph_35', 400, 'Regular', {Id = 'Graph_35.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Graph_35'))})),
+        Minecraftia = Font.new(custom_fonts.Trash_Bag:Register_Font('Minecraftia', 400, 'Regular', {Id = 'Minecraftia.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Minecraftia'))})),
+        ProggyCleanTT = Font.new(custom_fonts.Trash_Bag:Register_Font('ProggyCleanTT', 400, 'Regular', {Id = 'ProggyCleanTT.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyCleanTT'))})),
+        Smallest_Pixel = Font.new(custom_fonts.Trash_Bag:Register_Font('Smallest_Pixel', 400, 'Regular', {Id = 'Smallest_Pixel.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Smallest_Pixel'))})),
+        Templeos = Font.new(custom_fonts.Trash_Bag:Register_Font('Templeos', 400, 'Regular', {Id = 'Templeos.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/Templeos'))})),
+        ProggyTiny = Font.new(custom_fonts.Trash_Bag:Register_Font('ProggyTiny', 400, 'Regular', {Id = 'ProggyTiny.ttf',Font = crypt.base64.decode(game:HttpGet('https://raw.github.com/Blazoid/Trash/main/ProggyTiny'))})),
     }
 end
 
