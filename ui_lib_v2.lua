6432e0dc69e143ae6b77371185ec6f090cc063f1
Blazoid <tresh.lol@mail.ru>
2025-09-13 13:38:16 +0400

    Update ui_lib_v2.lua

--- ui_lib_v2.lua
+++ ui_lib_v2.lua
@@ -178,7 +178,6 @@ end;
 function Library:CreateLabel(Properties, IsHud)
     local _Instance = Library:Create('TextLabel', {
         BackgroundTransparency = 1;
-        Font = custom_fonts;
         TextColor3 = Library.FontColor;
         TextSize = 16;
         TextStrokeTransparency = 0;
