570b3586a361cff792e300795869190abe868396
Blazoid <tresh.lol@mail.ru>
2025-09-13 12:14:30 +0400

    Create sounds.lua

---
+++ storage/sounds.lua
@@ -0,0 +1,16 @@
+local sounds = {
+    ["Gamesense"] = "rbxassetid://4817809188",
+    ["Neverlose"] = "rbxassetid://8726881116",
+    ["Baimware"] = "rbxassetid://3124331820",
+    ["Steve"] = "rbxassetid://4965083997",
+    ["Among Us"] = "rbxassetid://5700183626",
+    ["Button"] = "rbxassetid://12221967",
+    ["Oof"] = "rbxassetid://4792539171",
+    ["Osu"] = "rbxassetid://7149919358",
+    ["Snow"] = "rbxassetid://6455527632",
+    ["Bambi"] = "rbxassetid://8437203821",
+    ["Click"] = "rbxassetid://8053704437",
+    ["Stone"] = "rbxassetid://3581383408",
+}
+
+return sounds
