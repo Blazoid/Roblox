26eb774191e31b03b900dc456e8ccd10423eef7d
Blazoid <tresh.lol@mail.ru>
2025-09-26 20:13:53 +0400

    Update ui_lib_v2.lua

--- ui_lib_v2.lua
+++ ui_lib_v2.lua
@@ -2672,7 +2672,7 @@ function Funcs:AddLabel(Text, DoesWrap)
 
         setmetatable(Depbox, BaseGroupbox);
 
-        table.insert(Library.DependencyBoxes[], Depbox);
+        table.insert(Library.DependencyBoxes, Depbox);
 
         return Depbox;
     end;
